46fbcdbd077116f53c1461026fdc870825d27cbe
ProjectGrantwood <justinphillipturner@gmail.com>
2023-03-15 13:00:25 -0400

    completed week02 lab

---
+++ week01/console.java
@@ -0,0 +1,8 @@
+package week01;
+
+public class console {
+    public static void main(String[] args) {
+        double a = (15 / 7) * 2;
+        System.out.println(a);
+    }
+}
--- week02/Week02BooleanConditionalsLoopsLab.java
+++ week02/Week02BooleanConditionalsLoopsLab.java
@@ -17,13 +17,15 @@ public class Week02BooleanConditionalsLoopsLab {
 		// 1. Variable Declaration:
 		//		a. Create a variable named age and assign it a value of 14
 
-		
+		short age = 14;
 		
 		// 2. Print a Boolean Expression:
 		//		a. Print the boolean expression age >= 16 to the console and note the results.
 		//		a. Change the value of age to 18 and print again.
 
-
+		System.out.println(age >= 16);
+		age = 18;
+		System.out.println(age >= 16);
 		
 		// 3. Can you drive?
 		//		a. Using a conditional, print one of the following:
@@ -31,8 +33,20 @@ public class Week02BooleanConditionalsLoopsLab {
 		//				ii. "You cannot drive" otherwise
 		//
 		//		a. Change the value of age and rerun to see the result
-	
-
+		
+		if (age >= 16){
+			System.out.println("You can drive");
+		} else {
+			System.out.println("You cannot drive");
+		}
+		
+		age = 14;
+		
+		if (age >= 16){
+			System.out.println("You can drive");
+		} else {
+			System.out.println("You cannot drive");
+		}
 		
 		// 4. Update Solution to Question 3 as follows:
 		//		a. Add a new variable called hasLicense before the conditional.
@@ -40,7 +54,14 @@ public class Week02BooleanConditionalsLoopsLab {
 		//				include the need for hasLicense to be true.
 		//		c. Try changing the values of age and hasLicense and note the different results.
 
+		boolean hasLicense = false;
+		age = 18;
 		
+		if (age >= 16 && hasLicense){
+			System.out.println("You can drive");
+		} else {
+			System.out.println("You cannot drive");
+		}
 		
 		// 5. Milk?
 		//		a. Create two new variables - costOfMilk and thirstLevel
@@ -48,7 +69,16 @@ public class Week02BooleanConditionalsLoopsLab {
 		//				or if thirstLevel is greater than 6 and prints "No Thanks" otherwise.
 		//		c. Change the values and note the different results.
 
-	
+		double costOfMilk = 2.49;
+		short thirstLevel = 3;
+		
+		if (costOfMilk < 2.5 && thirstLevel > 6){
+			System.out.println("Milk Please");
+		} else {
+			System.out.println("No Thanks");
+		}
+		
+		
 		
 		// 6. Cookie Distribution: 
 		//			Note:  You will evenly distribute all of the cookies to the children 
@@ -62,7 +92,19 @@ public class Week02BooleanConditionalsLoopsLab {
 		//				iii. If there are less than 5 cookies, print "Whoohoooo!"
 		//				iv. If there are 5 or more cookies, print "Jackpot!"
 
+		short numberOfCookies = 10;
+		short numberOfChildren = 6;
+		int cookiesRemaining = numberOfCookies % numberOfChildren;
 		
+		if (cookiesRemaining == 0){
+			System.out.println("Sad Face");
+		} else if (cookiesRemaining < 2){
+			System.out.println("Yes!");
+		} else if (cookiesRemaining < 5){
+			System.out.println("Whoohoooo!");
+		} else if (cookiesRemaining >= 5) {
+			System.out.println("Jackpot!");
+		}
 				
 		// 7.  Loyalty Member Program:
 		//		a. Create a variable called loyaltyMemberStatus and assign the value "SILVER"
@@ -73,7 +115,19 @@ public class Week02BooleanConditionalsLoopsLab {
 		//				ii. "GOLD" is 0.15
 		//				iii. "PLATINUM" is 0.25
 		
-
+		String LoyaltyMemberStatus = "SILVER";
+		double LoyaltyMemberDiscount = 0.0;
+		switch (LoyaltyMemberStatus){
+			case "SILVER":
+				LoyaltyMemberDiscount = 0.1;
+				break;
+			case "GOLD":
+				LoyaltyMemberDiscount = 0.15;
+				break;
+			case "PLATINUM":
+				LoyaltyMemberDiscount = 0.25;
+				break;
+		}
 		
 		// 8. Using the Loyalty Member Program variables from Question 7, do the following:
 		//		a. Create a variable called billTotal and assign a value
@@ -82,7 +136,22 @@ public class Week02BooleanConditionalsLoopsLab {
 		//		c. If the adjustedBillTotal is greater than $500 upgrade the 
 		//				loyaltyMemberStatus from SILVER to GOLD or from GOLD to PLATINUM
 
-
+		double billTotal = 19.64;
+		
+		double adjustedTotal = billTotal - billTotal * LoyaltyMemberDiscount;
+		
+		if (adjustedTotal > 500){
+			String newStatus = "";
+			switch (LoyaltyMemberStatus){
+				case "SILVER":
+					newStatus = "GOLD";
+					break;
+				case "GOLD":
+					newStatus = "PLATINUM";
+					break;
+			}
+			LoyaltyMemberStatus = newStatus;
+		}
 		
 		// 9. Login -- username & password:
 		//		a. Create two variables, username and password
@@ -90,7 +159,14 @@ public class Week02BooleanConditionalsLoopsLab {
 		//				i. "login successful" if the username is "Tommy123" and the password is "12345"
 		//				ii. "access denied" otherwise
 
+		String username = "Tommy123";
+		String password = "12345";
 		
+		if (username == "Tommy123" && password == "12345"){
+			System.out.println("Login successful");
+		} else {
+			System.out.println("access denied");
+		}
 		
 		//
 		// LOOPS:
@@ -98,25 +174,41 @@ public class Week02BooleanConditionalsLoopsLab {
 		
 		// 10. Write a for loop that prints each number from 0 to 9
 
-		
+		for (int i = 0; i < 10; i++){
+			System.out.println(i);
+		}
 		
 		// 11. Write a for loop that prints each number from 10 to 0 backwards
 
-		
+		for (int i = 10; i >= 0; i--){
+			System.out.println(i);
+		}
 		
 		// 12. Write a for loop that prints every other number from 0 to 100
 
-		
+		for (int i = 0; i <= 100; i+=2){
+			System.out.println(i);
+		}
 		
 		// 13. Write a for loop that iterates from 0 to 100 and prints 
 		//			"EVEN" if the number is even and "ODD" if it's odd
 
-		
+		for (int i = 0; i <= 100; i++){
+			if (i % 2 == 0){
+				System.out.println("EVEN");
+			} else {
+				System.out.println("ODD");
+			}
+		}
 		
 		// 14. Write a while loop that starts at 100 and iterates backwards by 1 until it reaches 0
 		//			within the loop, divide each number by 3 and print the remainder to the console.
-
 		
+		int i = 100;
+		while (i >= 0) {
+			System.out.println(i % 3);
+			i--;
+		}
 
 		
 	}
